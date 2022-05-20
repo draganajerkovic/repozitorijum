2e887debd788405891ef21ca57c46e0bb1153b45
Dragana <jerkovicdragana.jd@gmail.com>
2022-05-20 21:47:43 +0200

    jos jedna izmena

--- projekat20.05/src/Main.java
+++ projekat20.05/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        int a=3;
+        int b=5;
+        System.out.println(a*b);
+        System.out.println("Gittttttt");
     }
 }
